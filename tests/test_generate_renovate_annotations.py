0f568065281599f250c7401c907dd1bb2a3c87f6
mattkram <mkramer@anaconda.com>
2024-05-09 11:07:13 -0500

    Remove dummy test

--- tests/test_generate_renovate_annotations.py
+++ tests/test_generate_renovate_annotations.py
@@ -20,10 +20,6 @@ def environment_yaml():
     return yaml.safe_load(ENVIRONMENT_YAML)
 
 
-def test_the_tests():
-    assert True
-
-
 def test_load_environment_yaml(environment_yaml):
     assert environment_yaml == {
         "channels": ["defaults"],
